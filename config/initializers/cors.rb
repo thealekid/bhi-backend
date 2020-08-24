b107736cce6e0884295cf21fdadd8a37f8093755
Chuk Orakwusi <60194351+thealekid@users.noreply.github.com>
2020-08-24 16:10:37 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bhi.netlify.app/'
+    origins 'https://bhi.netlify.app'
 
     resource '*',
       headers: :any,
